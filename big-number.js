651203f260e2dcd49a8c98ae978a89c5f1df5e13
Jack Le Hamster <jacklehamster@gmail.com>
2021-08-14 23:38:15 -0700

    Update big-number.js

--- big-number.js
+++ big-number.js
@@ -1,12 +1,35 @@
 class BigNumberChunk {
   constructor() {
     this.value = 0;
-    this.next = null;
+    this.right = null;
+    this.left = null;
   }
 }
 
 class BigNumber {
     constructor() {
-      this.head = new BigNumberChunk();
+      this.tail = this.head = new BigNumberChunk();
+    }
+  
+    increment() {
+      this.tail.value++;
+      this.fixTail();
+    }
+  
+    fixTail() {
+      let node = this.tail;
+      while(node.value > 1000000) {
+        if (!node.left) {
+          node.left = new BigNumberChunk();
+          node.left.right = node;
+          this.head = node;
+        }
+        node.left.value += Math.floor(node.value / 1000000);
+        node.value = node.value % 1000000;
+        node = node.left;
+      }
+      while(this.head.value === 0 && this.head.next) {
+        this.head = this.head.next;
+      }
     }
 }
